f74813a3ac43e708a439f3ef140013c888a9f630
David Cui <davidcui@amazon.com>
2020-06-16 11:07:43 -0700

    adding light refactoring for scheduled reports table

--- public/components/main/scheduled_reports_table.js
+++ public/components/main/scheduled_reports_table.js
@@ -15,45 +15,42 @@
 
 import React from 'react';
 import {
-  EuiButton,
-  EuiPage,
   EuiLink
 } from '@elastic/eui';
-import { FormattedMessage } from '@kbn/i18n/react';
 
 export const scheduled_report_columns = [
-    {
-      field: 'reportName',
-      name: 'Name',
-      sortable: true,
-      truncateText: true,
-    },
-    {
-      field: 'lastUpdated',
-      name: 'Last Updated',
-      truncateText: true,
-    },
-    {
-      field: 'reportSource',
-      name: 'Source',
-      render: username => (
-        <EuiLink href={`https://github.com/${username}`} target="_blank">
-          {username}
-        </EuiLink>
-      ),
-    },
-    {
-      field: 'schedule',
-      name: 'Schedule',
-      truncateText: true,
-    },
+  {
+    field: 'reportName',
+    name: 'Name',
+    sortable: true,
+    truncateText: true,
+  },
+  {
+    field: 'lastUpdated',
+    name: 'Last Updated',
+    truncateText: true,
+  },
+  {
+    field: 'reportSource',
+    name: 'Source',
+    render: username => (
+      <EuiLink href={`https://github.com/${username}`} target="_blank">
+        {username}
+      </EuiLink>
+    ),
+  },
+  {
+    field: 'schedule',
+    name: 'Schedule',
+    truncateText: true,
+  },
 ];
 
 export const scheduled_reports = [
-    {
-        reportName: 'Daily Sales Report',
-        lastUpdated: 'April 21, 2020 @ 17:00:00',
-        reportSource: 'dashboard/daily_saves',
-        schedule: 'Daily at 17:00:00.000 starting April 1, 2020'
-    }
+  {
+    reportName: 'Daily Sales Report',
+    lastUpdated: 'April 21, 2020 @ 17:00:00',
+    reportSource: 'dashboard/daily_saves',
+    schedule: 'Daily at 17:00:00.000 starting April 1, 2020'
+  }
 ]
