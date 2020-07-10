24d9a64bdf8aba1cdb0cd466ffc859b21ed512c9
David Cui <53581635+davidcui-amzn@users.noreply.github.com>
2020-07-10 13:50:28 -0700

    Update Landing Page UI  (#37)
    Refactored landing page UI to reflect final UI. Fixed search functionalities and added filter capabilities as per user story, as well as snapshot test suite for all components.

---
+++ public/components/main/report_definitions_table.tsx
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+import React from 'react';
+import {
+  EuiLink,
+  EuiInMemoryTable,
+  EuiButton,
+  EuiEmptyPrompt,
+  EuiText,
+} from '@elastic/eui';
+import { report_definitions } from './test_data';
+
+export const scheduled_report_columns = [
+  {
+    field: 'reportName',
+    name: 'Name',
+    sortable: true,
+    truncateText: true,
+  },
+  {
+    field: 'type',
+    name: 'Type',
+    sortable: true,
+    truncateText: false,
+  },
+  {
+    field: 'owner',
+    name: 'Owner',
+    sortable: true,
+    truncateText: false,
+  },
+  {
+    field: 'source',
+    name: 'Source',
+    render: (username: string) => (
+      <EuiLink href={'#'} target="_blank">
+        {username}
+      </EuiLink>
+    ),
+  },
+  {
+    field: 'lastUpdated',
+    name: 'Last Updated',
+    truncateText: true,
+  },
+  {
+    field: 'details',
+    name: 'Details',
+    sortable: false,
+    truncateText: true,
+  },
+  {
+    field: 'status',
+    name: 'Status',
+    sortable: true,
+    truncateText: false,
+  },
+];
+
+const empty_message_report_definitions = (
+  <EuiEmptyPrompt
+    title={<h3>No report definitions to display</h3>}
+    titleSize="xs"
+    body={
+      <div>
+        <EuiText>Create a new report definition to get started</EuiText>
+        <EuiText>
+          To learn more, see{' '}
+          <EuiLink>Get started with Kibana reporting</EuiLink>
+        </EuiText>
+      </div>
+    }
+    actions={
+      <div>
+        <EuiButton
+          onClick={() => {
+            window.location.assign('opendistro-kibana-reports#/create');
+          }}
+        >
+          Create
+        </EuiButton>
+      </div>
+    }
+  />
+);
+
+const report_definitions_search = {
+  box: {
+    incremental: true,
+  },
+  filters: [],
+};
+
+export function ReportDefinitions(props) {
+  const { pagination } = props;
+  return (
+    <div>
+      <EuiInMemoryTable
+        items={report_definitions}
+        itemId="id"
+        loading={false}
+        message={empty_message_report_definitions}
+        columns={scheduled_report_columns}
+        search={report_definitions_search}
+        pagination={pagination}
+        sorting={true}
+        isSelectable={true}
+      />
+    </div>
+  );
+}
--- public/components/main/reports_table.tsx
+++ public/components/main/reports_table.tsx
@@ -13,12 +13,22 @@
  * permissions and limitations under the License.
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   EuiButton,
   // @ts-ignore
-  EuiLink
+  EuiLink,
+  EuiText,
+  EuiIcon,
+  EuiButtonIcon,
+  EuiEmptyPrompt,
+  EuiInMemoryTable,
+  EuiFlexGroup,
+  EuiSwitch,
+  EuiFlexItem,
+  EuiCheckbox,
 } from '@elastic/eui';
+import { reports_list_users } from './test_data';
 
 export const reports_list_columns = [
   {
@@ -28,102 +38,189 @@ export const reports_list_columns = [
     truncateText: true,
   },
   {
-    field: 'created',
-    name: 'Created',
+    field: 'type',
+    name: 'Type',
+    sortable: true,
+    truncateText: false,
+  },
+  {
+    field: 'sender',
+    name: 'Sender',
+    sortable: true,
+    truncateText: false,
+  },
+  {
+    field: 'recipients',
+    name: 'Recipient(s)',
+    sortable: true,
     truncateText: true,
   },
   {
     field: 'reportSource',
     name: 'Source',
     render: (username: string) => (
-      <EuiLink href={`https://github.com/${username}`} target="_blank">
+      <EuiLink href={'#'} target="_blank">
         {username}
       </EuiLink>
     ),
   },
   {
-    field: 'reportPDF',
-    name: 'File',
+    field: 'lastUpdated',
+    name: 'Last updated',
     truncateText: true,
+  },
+  {
+    field: 'state',
+    name: 'State',
+    sortable: true,
+    truncateText: false,
+  },
+  {
+    field: 'download',
+    name: 'Download',
+    sortable: false,
+    render: (download: any) => {
+      return <EuiButtonIcon iconType="download" />;
+    },
   },
 ];
 
-export const reports_list_users = [
-  {
-    id: 1,
-    reportName: 'Daily Sales Report, April 21',
-    created: 'April 21, 2020 @ 17:00:00',
-    reportSource: 'dashboard/daily_saves',
-    reportPDF: 'daily_sales_report_2020-04-21_17-e29f99ec3ce8.pdf'
-  },
-  {
-    id: 2,
-    reportName: 'Daily Sales Report, April 22',
-    created: 'April 22, 2020 @ 17:00:00',
-    reportSource: 'dashboard/daily_saves',
-    reportPDF: 'daily_sales_report_2020-04-22_17-0012395b51ed.pdf'
-  },
-  {
-    id: 3,
-    reportName: 'Daily Sales Report, April 23',
-    created: 'April 23, 2020 @ 17:00:00',
-    reportSource: 'dashboard/daily_saves',
-    reportPDF: 'daily_sales_report_2020-04-23_17-44cf47ed1283jd.pdf'
-  },
-]
-  const loadUsers = () => {};
-  const loadUsersWithError = () => {};
-  const loading = false;
-  const selection = [];
-  const onSelectionChange = () => {};
+const emptyMessageReports = (
+  <EuiEmptyPrompt
+    title={<h3>No reports to display</h3>}
+    titleSize="xs"
+    body={
+      <div>
+        <EuiText>
+          To get started, share or download a report from a dashboard,
+          visualization or saved search, or create a report definition
+        </EuiText>
+        <EuiText>
+          To learn more, see{' '}
+          <EuiLink>Get started with Kibana reporting</EuiLink>
+        </EuiText>
+      </div>
+    }
+  />
+);
 
-const renderToolsRight = () => {
-  return [
-    <EuiButton
-      key="loadUsers"
-      onClick={() => {
-        loadUsers();
-      }}
-      isDisabled={loading}>
-      Load Users
-    </EuiButton>,
-    <EuiButton
-      key="loadUsersError"
-      onClick={() => {
-        loadUsersWithError();
-      }}
-      isDisabled={loading}>
-      Load Users (Error)
-    </EuiButton>,
+export function ReportsTable(props) {
+  const { getRowProps, pagination } = props;
+
+  const [pageIndex, setPageIndex] = useState(0);
+  const [pageSize, setPageSize] = useState(10);
+  const [sortField, setSortField] = useState('lastUpdated');
+  const [sortDirection, setSortDirection] = useState('desc');
+
+  const onTableChange = ({ page = {}, sort = {} }) => {
+    const { index: pageIndex, size: pageSize } = page;
+
+    const { field: sortField, direction: sortDirection } = sort;
+
+    setPageIndex(pageIndex);
+    setPageSize(pageSize);
+    setSortField(sortField);
+    setSortDirection(sortDirection);
+  };
+
+  const sorting = {
+    sort: {
+      field: sortField,
+      direction: sortDirection,
+    },
+  };
+
+  const uniqueSenders = [];
+  reports_list_users.map((user) => {
+    if (uniqueSenders.indexOf(user.sender) === -1) {
+      uniqueSenders.push(user.sender);
+    }
+  });
+
+  const uniqueRecipients = [];
+  reports_list_users.map((user) => {
+    for (const [index, value] of user.recipients.entries()) {
+      if (uniqueRecipients.indexOf(value) === -1) {
+        uniqueRecipients.push(value);
+      }
+    }
+  });
+
+  const reports_status_options = [
+    'Created',
+    'Error',
+    'Pending',
+    'Shared',
+    'Archived',
   ];
-};
+  const reports_type_options = ['Schedule', 'Download', 'Alert'];
 
-const renderToolsLeft = () => {
-  if (selection.length === 0) {
-    return;
-  }
-
-  const onClick = () => {};
+  const reports_list_search = {
+    box: {
+      incremental: true,
+    },
+    filters: [
+      {
+        type: 'field_value_selection',
+        field: 'sender',
+        name: 'Sender',
+        multiselect: false,
+        options: uniqueSenders.map((user) => ({
+          value: user,
+          name: user,
+          view: user,
+        })),
+      },
+      {
+        type: 'field_value_selection',
+        field: 'recipients',
+        name: 'Recipients',
+        multiselect: false,
+        options: uniqueRecipients.map((user) => ({
+          value: user,
+          name: user,
+          view: user,
+        })),
+      },
+      {
+        type: 'field_value_selection',
+        field: 'state',
+        name: 'State',
+        multiselect: false,
+        options: reports_status_options.map((state) => ({
+          value: state,
+          name: state,
+          view: state,
+        })),
+      },
+      {
+        type: 'field_value_selection',
+        field: 'type',
+        name: 'Type',
+        multiselect: false,
+        options: reports_type_options.map((type) => ({
+          value: type,
+          name: type,
+          view: type,
+        })),
+      },
+    ],
+  };
 
   return (
-    <EuiButton color="danger" iconType="trash" onClick={onClick}>
-      Delete Users
-    </EuiButton>
+    <div>
+      <EuiInMemoryTable
+        items={reports_list_users}
+        itemId="id"
+        loading={false}
+        message={emptyMessageReports}
+        columns={reports_list_columns}
+        search={reports_list_search}
+        pagination={pagination}
+        sorting={sorting}
+        rowProps={getRowProps}
+        onChange={onTableChange}
+      />
+    </div>
   );
-};
-
-export const reports_list_search = {
-  toolsLeft: renderToolsLeft,
-  toolsRight: renderToolsRight,
-  box: {
-    incremental: true,
-  },
-  filters: [],
-};
-
-export const reports_list_selection_value = {
-  selectable: (user: { online: boolean; }) => user.online,
-  selectableMessage: (selectable: boolean) =>
-    !selectable ? 'User is currently offline' : undefined,
-  onSelectionChange: (selection: string[]) => onSelectionChange,
-};
+}
