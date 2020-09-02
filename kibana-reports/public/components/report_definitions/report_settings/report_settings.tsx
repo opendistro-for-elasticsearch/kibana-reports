233a8f24681a4ac6c3b58eb18cfa9179d03e219c
David Cui <53581635+davidcui-amzn@users.noreply.github.com>
2020-09-02 12:43:43 -0700

    Add Routes for Get Visualizations & Saved Search (#57)
    Added routes for getting all visualizations and saved searches, and merged all report source get APIs into one router

--- kibana-reports/public/components/report_definitions/report_settings/report_settings.tsx
+++ kibana-reports/public/components/report_definitions/report_settings/report_settings.tsx
@@ -151,6 +151,8 @@ export function ReportSettings(props) {
   const [visualizationSourceSelect, setVisualizationSourceSelect] = useState(
     REPORT_SOURCE_VISUALIZATION_OPTIONS[0].value
   );
+  const [visualizations, setVisualizations] = useState([]);
+
   const [savedSearchSourceSelect, setSavedSearchSourceSelect] = useState(
     REPORT_SOURCE_SAVED_SEARCH_OPTIONS[0].value
   );
@@ -164,6 +166,10 @@ export function ReportSettings(props) {
 
   const handleDashboards = (e) => {
     setDashboards(e);
+  };
+
+  const handleVisualizations = (e) => {
+    setVisualizations(e);
   };
 
   const handleReportName = (e: {
@@ -184,8 +190,12 @@ export function ReportSettings(props) {
     setReportSourceId(e);
     if (e === 'dashboardReportSource') {
       createReportDefinitionRequest['report_source'] = 'Dashboard';
+      createReportDefinitionRequest['report_params']['url'] =
+        getDashboardBaseUrl() + dashboards[0].value;
     } else if (e === 'visualizationReportSource') {
       createReportDefinitionRequest['report_source'] = 'Visualization';
+      createReportDefinitionRequest['report_params']['url'] =
+        getVisualizationBaseUrl() + visualizations[0].value;
     }
   };
 
@@ -201,6 +211,8 @@ export function ReportSettings(props) {
     target: { value: React.SetStateAction<string> };
   }) => {
     setVisualizationSourceSelect(e.target.value);
+    createReportDefinitionRequest['report_params']['url'] =
+      getVisualizationBaseUrl() + e.target.value;
   };
 
   const handleSavedSearchSelect = (e: {
@@ -328,7 +340,7 @@ export function ReportSettings(props) {
         <EuiFormRow label="Select visualization">
           <EuiSelect
             id="reportSourceVisualizationSelect"
-            options={REPORT_SOURCE_VISUALIZATION_OPTIONS}
+            options={visualizations}
             value={visualizationSourceSelect}
             onChange={handleVisualizationSelect}
           />
@@ -402,9 +414,31 @@ export function ReportSettings(props) {
     );
   };
 
+  const getVisualizationOptions = (data) => {
+    let index;
+    let options = [];
+    for (index = 0; index < data.length; ++index) {
+      let entry = {
+        value: data[index]['_id'].substring(14),
+        text: data[index]['_source']['visualization']['title'],
+      };
+      options.push(entry);
+    }
+    return options;
+  };
+
+  const getVisualizationBaseUrl = () => {
+    let baseUrl = window.location.href;
+    return baseUrl.replace(
+      'opendistro_kibana_reports#/create',
+      'kibana#/visualize/edit/'
+    );
+  };
+
   useEffect(() => {
+    // get dashboard options
     httpClientProps
-      .get('../api/reporting/getDashboards')
+      .get('../api/reporting/getReportSource/dashboard')
       .then(async (response) => {
         let dashboardOptions = getReportSettingDashboardOptions(
           response['hits']['hits']
@@ -420,6 +454,19 @@ export function ReportSettings(props) {
       });
     createReportDefinitionRequest['report_params']['report_format'] = 'pdf';
     createReportDefinitionRequest['report_source'] = 'Dashboard';
+
+    httpClientProps
+      .get('../api/reporting/getReportSource/visualization')
+      .then(async (response) => {
+        let visualizationOptions = getVisualizationOptions(
+          response['hits']['hits']
+        );
+        await handleVisualizations(visualizationOptions);
+        await setVisualizationSourceSelect(visualizationOptions[0].value);
+      })
+      .catch((error) => {
+        console.log('error when fetching visualizations:', error);
+      });
   }, []);
 
   return (
