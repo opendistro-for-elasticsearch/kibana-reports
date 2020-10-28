bb314e35cef1d9fbefe773214ec7ec1ea184051f
David Cui <53581635+davidcui-amzn@users.noreply.github.com>
2020-10-28 09:25:30 -0700

    Report definition details/Report details test coverage increase (#154)

--- kibana-reports/public/components/main/report_definition_details/__tests__/report_definition_details.test.tsx
+++ kibana-reports/public/components/main/report_definition_details/__tests__/report_definition_details.test.tsx
@@ -14,13 +14,19 @@
  */
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { act, render } from '@testing-library/react';
 import { ReportDefinitionDetails } from '../report_definition_details';
 import httpClientMock from '../../../../../test/httpMockClient';
 import 'babel-polyfill';
+import { configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+function setBreadcrumbs(array: []) {
+  jest.fn();
+}
 
 describe('<ReportDefinitionDetails /> panel', () => {
-  const propsMock = {
+  let propsMock = {
     match: {
       params: {
         reportDefinitionId: jest.fn(),
@@ -33,16 +39,371 @@ describe('<ReportDefinitionDetails /> panel', () => {
       reportDefinitionId: '1',
     },
   };
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-  test('render component', async (done) => {
+  configure({ adapter: new Adapter() });
+  test('render on demand definition details', async () => {
+    const promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: 'test create report definition trigger',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'On demand',
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
     const { container } = render(
       <ReportDefinitionDetails
         httpClient={httpClientMock}
         props={propsMock}
         match={match}
+        setBreadcrumbs={setBreadcrumbs}
       />
     );
     expect(container.firstChild).toMatchSnapshot();
-    done();
+    await act(() => promise);
+  });
+
+  test('render 5 hours recurring definition details', async () => {
+    const promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: 'test create report definition trigger',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 5,
+              unit: 'HOURS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: true,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    const { container } = render(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+    await act(() => promise);
+  });
+
+  test('render disabled daily definition, click', async () => {
+    let promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: 'test create report definition trigger',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 1,
+              unit: 'DAYS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: false,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    const { container } = render(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+
+    expect(container.firstChild).toMatchSnapshot();
+    await act(() => promise);
+  });
+
+  test('simulate click on generateReport', async () => {
+    let promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: null,
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'On demand',
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    const component = mount(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+    await act(() => promise);
+    component.update();
+    const statusButton = component.find('button').at(1);
+
+    statusButton.simulate('click');
+    await act(() => promise);
+  });
+
+  test('simulate click on delete', async () => {
+    let promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: null,
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 1,
+              unit: 'DAYS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: false,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    const component = mount(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+
+    const statusButton = component.find('button').at(0);
+    statusButton.update();
+    statusButton.simulate('click');
+
+    await act(() => promise);
+  });
+
+  test('simulate click to enable', async () => {
+    let promise = Promise.resolve();
+    const report_definition = {
+      status: 'Disabled',
+      report_params: {
+        report_name: 'test click on enable disable',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 1,
+              unit: 'DAYS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: false,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    httpClientMock.put = jest.fn().mockResolvedValue({});
+
+    const component = mount(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+    await act(() => promise);
+    component.update();
+    const statusButton = component.find('button').at(1);
+
+    statusButton.simulate('click');
+    await act(() => promise);
+  });
+
+  test('simulate click to disable', async () => {
+    let promise = Promise.resolve();
+    const report_definition = {
+      status: 'Active',
+      report_params: {
+        report_name: 'test click on enable disable',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 1,
+              unit: 'DAYS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: true,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
+    httpClientMock.put = jest.fn().mockResolvedValue({});
+
+    const component = mount(
+      <ReportDefinitionDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+        setBreadcrumbs={setBreadcrumbs}
+      />
+    );
+    await act(() => promise);
+    component.update();
+    const statusButton = component.find('button').at(1);
+
+    statusButton.simulate('click');
+    await act(() => promise);
   });
 });
--- kibana-reports/public/components/main/report_details/__tests__/report_details.test.tsx
+++ kibana-reports/public/components/main/report_details/__tests__/report_details.test.tsx
@@ -19,6 +19,7 @@ import { ReportDetails } from '../report_details';
 import propsMock from '../../../../../test/propsMock';
 import httpClientMock from '../../../../../test/httpMockClient';
 import 'babel-polyfill';
+import { act } from 'react-dom/test-utils';
 
 describe('<ReportDetails /> panel', () => {
   const match = {
@@ -27,7 +28,34 @@ describe('<ReportDetails /> panel', () => {
     },
   };
 
-  test('render component', async (done) => {
+  test('render on-demand component', async () => {
+    const promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: 'test create report definition trigger',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'On demand',
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+    });
+
     const { container } = await render(
       <ReportDetails
         httpClient={httpClientMock}
@@ -36,6 +64,58 @@ describe('<ReportDetails /> panel', () => {
       />
     );
     await expect(container.firstChild).toMatchSnapshot();
-    done();
+    await act(() => promise);
+  });
+
+  test('render 5 hours recurring component', async () => {
+    const promise = Promise.resolve();
+    const report_definition = {
+      report_params: {
+        report_name: 'test create report definition trigger',
+        report_source: 'Dashboard',
+        description: '',
+        core_params: {
+          base_url: '',
+          report_format: '',
+          header: '',
+          footer: '',
+          time_duration: '',
+        },
+      },
+      delivery: {
+        delivery_type: '',
+        delivery_params: {},
+      },
+      trigger: {
+        trigger_type: 'Schedule',
+        trigger_params: {
+          schedule_type: 'Recurring',
+          schedule: {
+            interval: {
+              period: 5,
+              unit: 'HOURS',
+              timezone: 'PST8PDT',
+            },
+          },
+          enabled_time: 1114939203,
+          enabled: true,
+        },
+      },
+    };
+
+    httpClientMock.get = jest.fn().mockResolvedValue({
+      report_definition,
+      query_url: `http://localhost:5601/app/kibana#/dashboard/7adfa750-4c81-11e8-b3d7-01146121b73d?_g=(time:(from:'2020-10-23T20:53:35.315Z',to:'2020-10-23T21:23:35.316Z'))`,
+    });
+
+    const { container } = await render(
+      <ReportDetails
+        httpClient={httpClientMock}
+        props={propsMock}
+        match={match}
+      />
+    );
+    await expect(container.firstChild).toMatchSnapshot();
+    await act(() => promise);
   });
 });
